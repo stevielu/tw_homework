a8ef45df6e89342e23a774a4cdf41845a7a1098f
stevie <luwei@huali-tec.com>
2019-08-23 18:01:24 +0800

    Feature - Network API

---
+++ RXNetworkPlatform/Api/UserNetwork.swift
@@ -0,0 +1,26 @@
+//
+//  UserNetwork.swift
+//  RXNetworkPlatform
+//
+//  Created by stevie on 2019/8/23.
+//  Copyright © 2019 vcon. All rights reserved.
+//
+
+import Model
+import RxSwift
+
+public final class UserNetwork{
+    private let network:Network<User>
+    
+    init(network:Network<User>) {
+        self.network = network
+    }
+    
+    public func fetchUser(userId:String) -> Observable<User>{
+        return self.network.getItem("", itemId: userId)
+    }
+    
+    public func fetchUsers() -> Observable<[User]>{
+        return self.network.getItems("")
+    }
+}
---
+++ RXNetworkPlatform/Network/NetworkProvider.swift
@@ -0,0 +1,19 @@
+//
+//  NetworkProvider.swift
+//  RXNetworkPlatform
+//
+//  Created by stevie on 2019/8/23.
+//  Copyright © 2019 vcon. All rights reserved.
+//
+
+import Model
+
+final class NetworkProvider{
+    private let apiEndpoint: String
+    
+    public init() {
+        apiEndpoint = "http://thoughtworks-ios.herokuapp.com/"
+    }
+    
+    
+}
